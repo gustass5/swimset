7160dad82a725f53b8631192a62dedfc07972da8
Gustas <gustass1@gmail.com>
2020-03-25 20:40:30 +0100

    feat(php): added basic login form

---
+++ php/login.php
@@ -0,0 +1,59 @@
+<?php
+
+session_start();
+
+require 'lib/password.php';
+
+require 'config.php';
+
+if(isset($_POST['login'])){
+
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+
+    $sql = 'SELECT id, username, password FROM users WHERE username = :username';
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindValue(':username', $username);
+
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($user === false){
+        die('Incorrect username / password combination!');
+    }else{
+       $validPassword = password_verify($passwordAttempt, $user['password']);
+    
+        if($validPassword){
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['logged_in'] = time();
+
+            header('Location: home.php');
+            exit;
+        }else{
+            die('Incorrect username / password combination!');
+        }
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <form action="login.php" method="post">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username"><br>
+            <label for="password">Password</label>
+            <input type="text" id="password" name="password"><br>
+            <input type="submit" name="login" value="Login">
+        </form>
+    </body>
+</html>
