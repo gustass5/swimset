21f0db80d99bd420730a14f95cc97cc34f71cabf
Gustas <gustass1@gmail.com>
2020-03-26 16:17:45 +0100

    feat(php: register): added form validation

--- php/register.php
+++ php/register.php
@@ -7,9 +7,72 @@ require 'lib/password.php';
 require 'config.php';
 
 if(isset($_POST['register'])){
-    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
-    $pass = !empty($_POST['password']) ? trim($_POST['password']): null;
+    try{
+        validateNewUser();
+        addNewUser();
+    }catch(Exception $error){
+        echo $error;
+    }
+}
+
+function validateNewUser(){
+    $username = !empty(trim($_POST['username'])) ? trim($_POST['username']) : null;
+    $currentPassword = !empty(trim($_POST['current-password'])) ? trim($_POST['current-password']) : null;
+    $confirmPassword = !empty(trim($_POST['confirm-password'])) ? trim($_POST['confirm-password']) : null;
     
+    $usernameExceptions = [];
+    $currentPasswordExceptions = [];
+    $confirmPasswordExceptions = [];
+
+    if($username === null){
+        $usernameExceptions[] = 'Username was not provided';
+    }
+
+    if(strpos($username, ' ') !== false ){
+        $usernameExceptions = 'Username must not have spaces';
+    }
+
+    if($currentPassword === null){
+        $currentPasswordExceptions[] = 'Password was not provided';
+    }
+
+    if(strpos($currentPassword, ' ') !== false){
+        $currentPasswordExceptions[] = 'Password must not have spaces';
+    }
+
+    if(preg_match("/[^A-Za-z0-9]/", $currentPassword)){
+        $currentPasswordExceptions[] = 'Password can only contain letters and numbers';
+    }
+
+    if(strlen($currentPassword) < 8){
+        $currentPasswordExceptions[] = 'Password must have at least 8 characters';
+    }
+
+    if(!preg_match('/[0-9]/', $currentPassword)){
+        $currentPasswordExceptions[] = 'Password must contain at least 1 number';
+    }
+
+    if($confirmPassword === null){
+        $confirmPasswordExceptions[] = 'Password confirmation was not provided';
+    }
+
+    if(strcmp($confirmPassword,$currentPassword)){
+        $confirmPasswordExceptions[] = 'Passwords do not match';
+    }
+
+    if(count($usernameExceptions) > 0 || count($currentPasswordExceptions) > 0 || count($confirmPasswordExceptions) > 0){
+            echo '<div>'.$usernameExceptions[0].' - '.$currentPasswordExceptions[0].' - '.$confirmPasswordExceptions[0].'</div>';
+        throw new Exception();
+    }
+
+}
+
+function addNewUser(){
+    Global $pdo;
+    return;
+    $username = trim($_POST['username']);
+    $password = trim($_POST['current-password']);
+
     $sql = 'SELECT COUNT(username) AS num FROM users WHERE username = :username';
     $stmt = $pdo->prepare($sql);
 
@@ -20,10 +83,10 @@ if(isset($_POST['register'])){
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if($row['num'] > 0){
-        die('That username already exists!');
+        throw new Exception('That username already exists!');
     }
 
-    $passwordHash = password_hash($pass, PASSWORD_BCRYPT, array('cost' => 12));
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
 
     $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
     $stmt = $pdo->prepare($sql);
@@ -65,7 +128,7 @@ if(isset($_POST['register'])){
                 <div id='confirm-password-error'></div>
 
             </div>
-            <input type="submit" name="register" value="Register"></button>
+            <input type="submit" name="register" value="Register">
         </form>
     </body>
 </html>
