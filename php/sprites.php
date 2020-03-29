b3c800ab735eec0da735df7b475d6fed2f3e4d41
Gustas <gustass1@gmail.com>
2020-03-29 14:53:16 +0200

    feat(php): added sprite search

--- php/sprites.php
+++ php/sprites.php
@@ -3,14 +3,24 @@
 require 'config.php';
 require 'helpers.php';
 
-define('ITEMS_PER_ROW', 3);
+$searchValue = !empty(trim($_GET['search_sprite'])) ? trim($_GET['search_sprite']) : null;
 
-$sql = 'SELECT * FROM sprites';
+if($searchValue !== null){
+  $sql = 'SELECT * FROM sprites WHERE title LIKE :search_text OR description LIKE :search_text2';
 
-$statement = $pdo->prepare($sql);
+  $statement = $pdo->prepare($sql);
+  $statement->bindValue(':search_text', '%' . $searchValue . '%');
+  $statement->bindValue(':search_text2', '%' . $searchValue . '%');
+
+}else{
+  $sql = 'SELECT * FROM sprites';
+  $statement = $pdo->prepare($sql);
+}
+
 
 $statement->execute();
 $sprites = $statement->fetchAll();
+
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +45,12 @@ $sprites = $statement->fetchAll();
               d="M1.004 5.998l10.996-5.998 10.99 6.06-10.985 5.86-11.001-5.922zm11.996 7.675v10.327l10-5.362v-10.326l-10 5.361zm-2 0l-10-5.411v10.376l10 5.362v-10.327z"
             />
           </svg>
-          <input class="flex-grow" type="text" name="searchbar" />
+          <form id="search-form" class="flex flex-grow" action='/swimset/php/sprites.php' method='get'>
+            <input id="search-bar" class="flex-grow" type="text" name="search_sprite" placeholder="Search..." />
+            <button class='px-1 search' type="submit">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path class="fill-current" d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
+            </button>
+          </form>
           <nav class="flex items-center">
             <a href="php/sprites.php">
               <div class="navigation-item">
@@ -54,33 +69,28 @@ $sprites = $statement->fetchAll();
               <div class="navigation-item">
                 <a href="users.php"><?php echo htmlspecialchars($user['username']);?></a>
               </div>
-            <?php }else{?>
-              <div class="sprite-title">
-                <span><?php echo ucfirst($user['role'])?></span>
-                <?php echo htmlspecialchars($user['username']);?>
-              </div>
             <?php }?>
             <a class="navigation-item" href="logout.php?token=<?= $_SESSION['user_token'] ?>">Logout</a>
           <?php }else{?>
           <a href="php/login.php">
-            <button class="navigation-item">
+            <div class="navigation-item">
               Login
-            </button>
+            </div>
           </a>
           <a href="php/register.php">
-            <button class="navigation-item">
+            <div class="navigation-item">
               Sign up
-            </button>
+            </div>
           </a>
           <?php }?>
     </header>
     <main class='grid-container mx-10'>
       <?php foreach($sprites as $sprite){ ?>
               <div class="relative sprite-listing shadow bg-white">
-                <a class="sprite-title flex-col" href="#"><?php echo htmlspecialchars($sprite['title'])?><svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-2" viewBox="0 0 24 24"><path d="M12.015 7c4.751 0 8.063 3.012 9.504 4.636-1.401 1.837-4.713 5.364-9.504 5.364-4.42 0-7.93-3.536-9.478-5.407 1.493-1.647 4.817-4.593 9.478-4.593zm0-2c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 5c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2zm0-2c-2.209 0-4 1.792-4 4 0 2.209 1.791 4 4 4s4-1.791 4-4c0-2.208-1.791-4-4-4z"/></svg></a>
+                <a class="sprite-title flex-col capitalize" href="../single-sprite.php?id=<?php echo htmlspecialchars($sprite['id']) ?>"><?php echo htmlspecialchars($sprite['title'])?><svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-2" viewBox="0 0 24 24"><path d="M12.015 7c4.751 0 8.063 3.012 9.504 4.636-1.401 1.837-4.713 5.364-9.504 5.364-4.42 0-7.93-3.536-9.478-5.407 1.493-1.647 4.817-4.593 9.478-4.593zm0-2c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 5c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2zm0-2c-2.209 0-4 1.792-4 4 0 2.209 1.791 4 4 4s4-1.791 4-4c0-2.208-1.791-4-4-4z"/></svg></a>
                 <div class="sprite-image">
-                  <a href="#">
-                    <img class="w-full h-auto" src="../uploads/<?php echo $sprite['path']?>" />
+                  <a href="../single-sprite.php?id=<?php echo htmlspecialchars($sprite['id']) ?>">
+                    <img class="w-full h-auto" src="../uploads/<?php echo htmlspecialchars($sprite['path'])?>" />
                   </a>
                 </div>
               </div>
