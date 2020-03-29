55b30d7ba5c8b9bf3f94f32c60d3e821b951ad1c
Gustas <gustass1@gmail.com>
2020-03-29 01:27:27 +0100

    chore: exported user-logged-in check

--- php/home.php
+++ php/home.php
@@ -1,22 +1,18 @@
 <?php
 
 session_start();
- 
- 
-/**
- * Check if the user is logged in.
- */
-if(!isset($_SESSION['user_id']) || !isset($_SESSION['logged_in'])){
-    //User not logged in. Redirect them back to the login.php page.
-    header('Location: login.php');
-    exit;
-}
- 
- 
-/**
- * Print out something that only logged in users can see.
- */
- 
-echo 'Congratulations! You are logged in!';
+require "helpers.php";
 
+checkLoggedIn();
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        	
+<a href="logout.php?token=<?= $_SESSION['user_token'] ?>">Logout</a>
+    </body>
+</html>
