fc8df832812b63121cbd7efe4cd30322d991e035
Gustas <gustass1@gmail.com>
2020-03-29 01:28:35 +0100

    feat(php): added ability to download sprites

---
+++ php/download-sprite.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['download'])){
+    $path = "../uploads/".$_POST['path'];
+    
+    if (file_exists($path)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($path).'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($path));
+        readfile($path);
+        exit;
+    }
+}
+?>
