711b02171d6b52289cee2e9e398cd2c04bd19f68
Gustas <gustass1@gmail.com>
2020-03-29 01:24:23 +0100

    feat(php): added logout functionality

---
+++ php/logout.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if(!function_exists('hash_equals')){
+    function hash_equals($str1, $str2){
+        if(strlen($str1) != strlen($str2)) {
+            return false;
+        } else {
+            $res = $str1 ^ $str2;
+            $ret = 0;
+            for($i = strlen($res) - 1; $i >= 0; $i--){
+                $ret |= ord($res[$i]);
+            }
+            return !$ret;
+        }
+    }
+}
+
+$queryStrToken = isset($_GET['token']) ? $_GET['token'] : '';
+
+if(hash_equals($_SESSION['user_token'], $queryStrToken)){
+    session_destroy();
+    header('Location: login.php');
+    exit;
+}
+
+?>
